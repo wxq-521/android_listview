38080d9fc9209154ae8f9e3a1d25c8609db3775f
Mr.xie <986713575@qq.com>
2016-07-08 17:34:31 +0800

    布局上进行了一下修改

--- gen/com/xie/day10_listviewpage_internet/R.java
+++ gen/com/xie/day10_listviewpage_internet/R.java
@@ -25,12 +25,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int food_str=0x7f080004;
-        public static final int foodshow=0x7f080000;
-        public static final int pic=0x7f080002;
-        public static final int tips=0x7f080001;
-        public static final int title=0x7f080003;
+        public static final int action_settings=0x7f080006;
+        public static final int app_title=0x7f080000;
+        public static final int food_str=0x7f080005;
+        public static final int foodshow=0x7f080001;
+        public static final int pic=0x7f080003;
+        public static final int tips=0x7f080002;
+        public static final int title=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
